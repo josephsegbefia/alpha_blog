346975bcf0e7df43c8d8772ad7f89d78d88e2eb5
Joseph Segbefia <elorma.segbefia@gmail.com>
2022-05-19 19:09:10 +0200

    Complete Destroy method and Refactor Code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show 
-        @article = Article.find(params[:id])
+        #call before_action for show
     end
 
     def index 
@@ -11,9 +13,13 @@ class ArticlesController < ApplicationController
         @article = Article.new 
     end
 
+    def edit
+        #call  before_action for edit
+    end 
+
     def create
     #   render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save 
             flash[:notice] = 'Article was created successfully.'
             redirect_to @article
@@ -21,4 +27,29 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end 
+
+    def update 
+        
+        if @article.update(article_params)
+            flash[:notice] = 'Article was updated successfully'
+            redirect_to @article 
+        else 
+            render 'edit'
+        end
+    end
+
+    def destroy 
+        
+        @article.destroy 
+        redirect_to articles_path
+    end
+
+    private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params 
+        params.require(:article).permit(:title, :description)
+    end
 end
